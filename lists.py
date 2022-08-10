cc5567fd77f8f5589200e468a5e92317f8c7aa3b
Mujtaba Sultani <mujtaba.cs01@gmail.com>
2022-08-10 18:30:34 +0430

    Python lists and tuples are added.

---
+++ lists.py
@@ -0,0 +1,11 @@
+
+exp = [30, 50, 100, 10, 500]
+
+sum = 0
+for items in exp:
+    sum = sum + items
+    print(sum)
+
+
+total = exp[0] + exp[1] + exp[2] + exp[3] + exp[4]
+print("Total expenses is: ",total)
---
+++ tuples.py
@@ -0,0 +1,6 @@
+point = (5,6)
+print(point[0])
+
+
+address = ("Ahmad", "Bagh Bala", "2nd district", "Kabul-Afghanistan")
+print(address)
